850c6d6d0c230dee42c2c6b634ae82a02e7ccefb
Atom656 <Zubko03y@gmail.com>
2023-02-11 13:44:13 +0200

    commit

---
+++ js/fade-slider.js
@@ -0,0 +1,13 @@
+(function() {
+    const sliders = document.querySelectorAll('.fade-slider__item');
+    const activeClass = "fade-slider__item--visible";
+    let index = 0;
+    setInterval(function() {
+        sliders[index].classList.remove(activeClass);
+        index++;
+        if(index + 1 > sliders.length){
+            index = 0;
+        }
+        sliders[index].classList.add(activeClass);
+    }, 7000);
+} ())
